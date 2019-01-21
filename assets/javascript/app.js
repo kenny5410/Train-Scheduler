95cb0cec7d08a70e03d758f9adf0d81e4d07aa91
Kenny <nels5410@gmail.com>
2019-01-21 17:37:03 -0600

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyC_bdfZ0FTkXGro6XdVSvxz5uPdQjM92L0",
+      authDomain: "ken-train-scheduler.firebaseapp.com",
+      databaseURL: "https://ken-train-scheduler.firebaseio.com",
+      projectId: "ken-train-scheduler",
+      storageBucket: "ken-train-scheduler.appspot.com",
+      messagingSenderId: "646081648283"
+    };
+    firebase.initializeApp(config);
+})
