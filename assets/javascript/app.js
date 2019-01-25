5a5104a4888b2a35da208971905d958352e37b8d
Kenny <nels5410@gmail.com>
2019-01-24 20:42:58 -0600

    added data to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,35 @@
-$( document ).ready(function(){
-  // Initialize Firebase
+
+// Initialize Firebase
   var config = {
-      apiKey: "AIzaSyC_bdfZ0FTkXGro6XdVSvxz5uPdQjM92L0",
-      authDomain: "ken-train-scheduler.firebaseapp.com",
-      databaseURL: "https://ken-train-scheduler.firebaseio.com",
-      projectId: "ken-train-scheduler",
-      storageBucket: "ken-train-scheduler.appspot.com",
-      messagingSenderId: "646081648283"
-    };
-    firebase.initializeApp(config);
+  apiKey: "AIzaSyC_bdfZ0FTkXGro6XdVSvxz5uPdQjM92L0",
+  authDomain: "ken-train-scheduler.firebaseapp.com",
+  databaseURL: "https://ken-train-scheduler.firebaseio.com",
+  projectId: "ken-train-scheduler",
+  storageBucket: "ken-train-scheduler.appspot.com",
+  messagingSenderId: "646081648283"
+};
+firebase.initializeApp(config);
+
+//variables
+var database = firebase.database();
+
+
+
+//Click function
+$('#submit').on('click', function(event) {
+  event.preventDefault();
+  
+  var name = $('#train-name-form').val().trim();
+  var destination = $('#destination-name-form').val().trim();
+  var time = $('#first-train-time-form').val().trim();
+  var frequency = $('#frequency-form').val().trim();
+
+  database.ref().push({
+    name: name,
+    destination: destination,
+    time: time,
+    frequency: frequency
+  })
 })
+
+
